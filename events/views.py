b897385903681050d69ffe912520e5c23589ed55
Gabriel Santos <gabrielsantos98898@gmail.com>
2022-01-26 15:53:44 -0300

    Criacao do projeto, configuracao do projeto, adicao do calendario no projeto

---
+++ events/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
+
+def home(request, year, month):
+    #deixa a primeira letra maiuscula
+    month = month.capitalize()
+
+    #Conversao de string do mes para inteiro
+    month_number = list(calendar.month_name).index(month)
+    month_number = int(month_number)
+
+    #Criacao do Calendario
+    cal = HTMLCalendar().formatmonth(year, month_number)
+
+    #Pega a data atual
+    now = datetime.now()
+    current_year = now.year
+
+    #Pega a hora atual
+    time = now.strftime('%I:%M %p') #formato do horario: 07:32 AM
+    #time = now.strftime('%H:%M:%S') #formato do horario: 17:42:55
+
+    context = {
+        "year": year,
+        "month": month,
+        "month_number": month_number,
+        "cal": cal,
+        "current_year": current_year,
+        "time": time
+    }
+
+    return render(request, 'home.html', context)
