f695ce8036f0b81851ef2aa2bb30aaae813b8768
Gabriel Santos <gabrielsantos98898@gmail.com>
2022-01-31 11:59:44 -0300

    Criacao de novas tabelas

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+admin.site.register(models.Venue)
+admin.site.register(models.myClubUser)
+admin.site.register(models.Event)
+
--- events/models.py
+++ events/models.py
@@ -1,3 +1,35 @@
+from pyexpat import model
 from django.db import models
 
-# Create your models here.
+class Venue(models.Model):
+    name = models.CharField("Venue name", max_length=120)
+    address = models.CharField(max_length=300)
+    zip_code = models.CharField('Zip code', max_length=15)
+    phone = models.CharField('contactphone', max_length=25)
+    web = models.URLField('Website Adress' )
+    email_address = models.EmailField('Email address')
+
+    def __str__(self):
+        return self.name
+
+class myClubUser(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField('User email')
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+
+class Event(models.Model):
+    name = models.CharField('Event name', max_length=120)
+    event_date= models.DateTimeField('Event Date')
+    venue = models.ForeignKey(Venue, blank=True, null=True, on_delete=models.CASCADE)
+    #venue = models.CharField(max_length=120)
+    manager = models.CharField(max_length=60)
+    description = models.TextField(blank=True)
+    attendees = models.ManyToManyField(myClubUser, blank=True) # Aqui é uma chave muitos para muitos
+
+    def __str__(self):
+        return self.name
+
