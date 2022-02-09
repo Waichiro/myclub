80041ce6af12e1a3da1d09f4c06d896585a57bc6
Gabriel Santos <gabrielsantos98898@gmail.com>
2022-02-09 10:33:46 -0300

    Aqui foi feita a parte de logout

--- members/views.py
+++ members/views.py
@@ -3,19 +3,24 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
+#Aqui é a parte de login
 def login_user(request):
 
     if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
+        username = request.POST['username'] #Ele pega o name do campo da tag la no html q tem esse msm nome 
+        password = request.POST['password'] #Ele pega o name do campo da tag la no html q tem esse msm nome 
+        user = authenticate(request, username=username, password=password) #Aqui ele faz a authenticacao para ver se existe e se ta tudo certo
+        if user is not None: #Aqui se tiver algo escrito e tiver passado pela authenticacao ele vai fazer o login
             login(request, user)
             return redirect('home')
         else:
-            messages.success(request, ("There Was An Error Loggin In, Try Again..."))
+            messages.success(request, ("There Was An Error Loggin In, Try Again...")) #Mensagem de erro caso o usuario nn exista ou esteja algo errado
             return redirect('login')
     else:
         return render(request, 'authenticate/login.html')
 
-
+#Aqui é a parte de logout
+def logout_user(request):
+    logout(request)#FAz o logout
+    messages.success(request, ("You Were Logged Out!!"))#Mensagem indicando q fez o logout
+    return redirect('home')
